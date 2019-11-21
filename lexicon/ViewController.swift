97bc16db773f4a0570270cf361c558f9ebb7411f
Pierce Kotarski <pjkotarski@gmail.com>
2019-11-21 14:14:04 -0600

    First commit

---
+++ lexicon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lexicon
+//
+//  Created by Pierce Kotarski on 11/21/19.
+//  Copyright © 2019 piercejk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
